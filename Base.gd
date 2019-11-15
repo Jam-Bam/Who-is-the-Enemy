c544b62baeb2ef77a3db0cba41613a12f19780b3
MoidHubber <kevincaldeira86@gmail.com>
2019-11-14 20:26:04 -0500

    added font and label

--- Base.gd
+++ Base.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var NPC_Resource = load("res://NPCs/NPC.tscn")
+onready var pop_label = $CanvasLayer/popLabel
+var population = 50
 
 func _ready():
-	for i in range(100):
+	for i in range(population):
 		var NPC = NPC_Resource.instance()
 		add_child(NPC)
 		NPC.position = Vector2(350,50)
+
+func _process(delta):
+	pop_label.text = "Population: " + String(population)
