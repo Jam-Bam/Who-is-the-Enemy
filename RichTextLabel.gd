ccb27ee7dcd35bfa3709b2ed3b0d50297ecc3a2d
Benjamin Hunsicker <benhunsicker1@gmail.com>
2019-11-15 15:51:33 -0500

    e

--- RichTextLabel.gd
+++ RichTextLabel.gd
@@ -12,26 +12,34 @@ func _ready():
 	
 
 func _dialogue(string):
+	$Timer2.start()
 	for letter in string:
 		$Timer.start()
 		$RichTextLabel.add_text(letter)
 		yield($Timer, "timeout")
-	$Timer2.start()
+	
 
 func _dialogue2(string):
+	$Timer3.start()
 	for letter in string:
 		$Timer.start()
 		$RichTextLabel.add_text(letter)
 		yield($Timer, "timeout")
-	$Timer3.start()
+	
 
 func _dialogue3(string):
+	$Timer4.start()
 	for letter in string:
 		$Timer.start()
 		$RichTextLabel.add_text(letter)
 		yield($Timer, "timeout")
-	$Timer4.start()
-
+	
+func _dialogue4(string):
+	$Timer5.start()
+	for letter in string:
+		$Timer.start()
+		$RichTextLabel.add_text(letter)
+		yield($Timer, "timeout")
 
 
 
@@ -44,15 +52,15 @@ func _on_Timer2_timeout():
 func _on_Timer3_timeout():
 	$RichTextLabel.text = ""
 	$AnimatedSprite.frame = 2
-	var sp = "When we found you, the radio didn;t have any batteries..."
-	_dialogue3(sp)
+	var sp2 = "When we found you, the radio didn;t have any batteries..."
+	_dialogue3(sp2)
 
 
 func _on_Timer4_timeout():
 	$RichTextLabel.text = ""
 	$AnimatedSprite.frame = 1
-	var sp = "You wouldn't understand..."
-	$Timer5.start()
+	var sp3 = "You wouldn't understand..."
+	_dialogue4(sp3)
 
 
 func _on_Timer5_timeout():
