9e90d2cae136a61b24d0390132ea4b91bc4a68dc
Daver1012 <Daver1012@users.noreply.github.com>
2019-11-15 11:02:48 -0500

    Added idle

--- NPCs/NPC.gd
+++ NPCs/NPC.gd
@@ -6,7 +6,7 @@ var movetimer_length = int(rand_range(100, 300))
 var movetimer = 0
 var anim = 0
 func _ready():
-	anim = int(rand_range(0,9))
+	anim = int(rand_range(0,19))
 	
 func movement_loop():
 	var motion = movedir.normalized() * SPEED
@@ -15,9 +15,17 @@ func movement_loop():
 	$AnimatedSprite.play(String(anim))
 	
 func _physics_process(delta):
+	if movedir != Vector2(0,0):
+		if $AnimatedSprite.frame == 2:
+			$AnimatedSprite.frame = 0
 	movement_loop()
 	if movetimer > 0:
 		movetimer -= 1
+	if movedir == Vector2(0,0):
+		$AnimatedSprite.stop()
+		$AnimatedSprite.frame = 2
+	else:
+		$AnimatedSprite.play()
 	if movetimer == 0 || is_on_wall():
 		movedir = dir.rand()
 		movetimer = movetimer_length
